ca31158f28b2fe11cce9b144e59b01fb49b97692
emmanuel.leparquier <emmanuel.leparquier@wister.fr>
2017-06-17 11:20:39 +0200

    Config file

---
+++ config/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Fr\Nj2\Api\Config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: manu
+ * Date: 17/06/17
+ * Time: 11:17
+ */
+class Config
+{
+    const DB_CREDENTIALS = [
+        [
+            'host'=>...,
+            'user'=>...,
+            'password'=>...,
+            'db'=>...
+        ]
+    ];
+}
